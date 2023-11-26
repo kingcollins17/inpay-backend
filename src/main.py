9127e1f7a87d248c66eb0d23f6f6d87bb8dfc8d1
kingcollins17 <98811110+kingcollins17@users.noreply.github.com>
2023-11-26 07:38:10 +0100

    Added Fetch User Endpoint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Depends
-from typing import Any, List, Union
+from typing import Any, List, Union, Annotated
+
+from .util import authenticate
+from .models import *
 from .routes.auth import router as auth_router
 from .routes.accounts import router as account_router
 from .routes.admin import router as admin_router
@@ -10,5 +13,5 @@ app.include_router(auth_router, prefix="/auth")
 app.include_router(account_router, prefix="/accounts")
 app.include_router(admin_router, prefix="/admin")
 @app.get('/')
-async def index():
-     return {'message': 'Welcome'}
+async def index(user: Annotated[User, Depends(authenticate)]):
+     return user
