1068949a1e0c0532de1ae37f335ef872d69e36d3
kingcollins17 <98811110+kingcollins17@users.noreply.github.com>
2023-12-30 20:56:23 +0100

    Production mode on

--- src/_config.py
+++ src/_config.py
@@ -1,2 +1,11 @@
+
+#Whether app is in production mode
+production = True
+
 db_credentials  = {'user': 'root', 'host': 'localhost', 'port': 3306, 
                 'password': 'mysqlking@02', 'db': 'inpay'}
+
+if production:
+     db_credentials = {'user': 'master', 'host': 'mysql-2c185947-kingcollins172-7f3a.a.aivencloud.com', 'port': 10826, 
+                'password': 'masterpass', 'db': 'inpay'}
+     
